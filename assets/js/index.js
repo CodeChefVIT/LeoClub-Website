6f3a789b4e11fe78cc5576f2e0f1012dfc095e38
Aarushi21 <shresthaaarushi@gmail.com>
2020-07-04 00:51:44 +0530

    coordinator section changed

--- assets/js/index.js
+++ assets/js/index.js
@@ -69,6 +69,16 @@ $(function(){
   
   });
 
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: false,
+    autoplayTimeout: 3000,
+    dots: false,
+    nav: true,
+    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+    responsive: responsive
+  });
+
   // AOS Animations
   AOS.init();
   
