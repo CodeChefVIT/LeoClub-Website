7630d6a6b69d71f392ce5772e9a6d13205fc38bb
Aarushi21 <shresthaaarushi@gmail.com>
2020-07-05 20:05:24 +0530

    improvements

--- assets/js/index.js
+++ assets/js/index.js
@@ -13,76 +13,15 @@
         loader();
     });
 
+    //navbar
+    
+
     // AOS Animations
     AOS.init();
 
-
-$(function(){
-
-    window.sr = ScrollReveal();
-  
-    if ($(window).width() < 768) {
-  
-        if ($('.timeline-content').hasClass('js--fadeInLeft')) {
-            $('.timeline-content').removeClass('js--fadeInLeft').addClass('js--fadeInRight');
-        }
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    } else {
-        
-        sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-    }
-    
-    sr.reveal('.js--fadeInLeft', {
-          origin: 'left',
-          distance: '300px',
-            easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-        sr.reveal('.js--fadeInRight', {
-          origin: 'right',
-          distance: '300px',
-          easing: 'ease-in-out',
-          duration: 800,
-        });
-  
-  
-  });
-
-  $('.owl-carousel').owlCarousel({
-    loop: true,
-    autoplay: false,
-    autoplayTimeout: 3000,
-    dots: false,
-    nav: true,
-    navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
-    responsive: responsive
-  });
-
-  // AOS Animations
-  AOS.init();
-  
-  
-  
-
 })(jQuery);
+
+$('nav > .navbar-collapse > ul > li > a').on('click',function(){
+  $('navbar-collapse').collapse('hide');
+});
+
